2e3f9555f8c0607239c84fe845bc0efe8a134415
Kri0819 <tina2013miao@gmail.com>
2026-06-11 23:41:19 +0800

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -4,3 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+
+
+vercel.json
+
+{
+  "rewrites": [{ "source": "/(.*)", "destination": "/index.html" }]
+}
